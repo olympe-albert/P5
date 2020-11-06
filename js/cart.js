30742bc5e741e7b2b4622c0b0544fc78bfb3841b
Olympe Albert <melinee.gerin@gmail.com>
2020-11-06 10:39:41 +0100

    ajout validation form et POST sur cart.js

--- js/cart.js
+++ js/cart.js
@@ -29,7 +29,7 @@ function showCart()
                     return response.json();
                 }).then(ids =>
                     {
-                        // affiche les produits
+                        // affiche les produits et leur prix
                         let productLabel = document.createElement("p");
                         let cart = document.getElementById("cart");
                         cart.appendChild(productLabel);
@@ -72,4 +72,108 @@ setTimeout (function()
     let cart = document.getElementById("cart");
     cart.appendChild(totalPrice);
     totalPrice.innerHTML = "Prix total : " + lastPrice + "€";
+// stockage du montant de la commande
+    localStorage.setItem("orderAmount", lastPrice);
+    console.log("Montant commande stocké : " + lastPrice);
 }, 100);
+
+// formulaire de contact
+
+// déclaration des regex
+let regexText = /^[A-Za-zçàêéèîïÀÊÉÈÎÏ\s-]{2,}$/;
+let regexAddress = /^[A-Za-zçàêéèîïÀÊÉÈÎÏ0-9\s-]{2,}$/;
+let regexMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)*$/;
+
+// récupération des champs du formulaire
+let firstName = document.getElementById("firstname");
+let lastName = document.getElementById("lastname");
+let address = document.getElementById("address");
+let city = document.getElementById("city");
+let mail = document.getElementById("mail");
+
+// création du tableau de produits
+let products = [];
+let ids = JSON.parse(cartContent);
+products.push(ids);
+
+// fonction de validation du formulaire
+function validateForm()
+{
+    // si un champ requis est manquant
+    if(firstName.validity.valueMissing || lastName.validity.valueMissing || address.validity.valueMissing || city.validity.valueMissing || mail.validity.valueMissing)
+    {
+        let alert = document.getElementById("alert");
+        alert.innerHTML = "Veuillez remplir tous les champs.";
+        console.log("Missing value here");
+        return false;
+    }
+    // si un champ est mal rempli
+    else if(regexText.test(firstName.value) == false || regexText.test(lastName.value) == false || regexAddress.test(address.value) == false || regexText.test(city.value) == false || regexMail.test(mail.value) == false)
+    {
+        let alert = document.getElementById("alert");
+        alert.innerHTML = "Veuillez remplir les champs correctement.";
+        console.log("Wrong value here");
+        return false;
+    }
+    else
+    {
+        let alert = document.getElementById("alert");
+        alert.innerHTML = "Merci !";
+        return true;
+    }
+};
+
+// fonction de création de contact
+function createContact()
+{
+    let contact = {
+    firstName : firstName.value, 
+    lastName : lastName.value, 
+    address : address.value, 
+    city : city.value, 
+    email : mail.value
+    };
+    return contact;
+}
+
+// fonction POST products et contact à l'API
+function sendAndReceiveData(contact, products)
+{
+    let fetchPostPromise = fetch("http://localhost:3000/api/cameras/order",
+    {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({contact, products})
+    });
+    fetchPostPromise.then(response =>
+        {
+            return response.json(); 
+        }).then(backData => 
+            {
+            console.log("Numéro de commande :" + backData.orderId);
+            localStorage.setItem("orderId", backData.orderId);
+            });
+};
+
+// au clic sur le bouton d'envoi...
+
+let sendInput = document.getElementById("send");
+sendInput.addEventListener('click', function(e)
+{
+    // ... validation du formulaire
+    validateForm(); 
+    e.preventDefault();
+    // ... création d'un objet contact et POST à l'API
+    if(validateForm()==true)
+    {
+    console.log("Formulaire validé");
+    contact = createContact();
+    console.log("Contact créé");
+    sendAndReceiveData(contact, products);
+    window.location = "./order.html"
+    }
+    else
+    {
+        console.log("Formulaire non-validé");
+    }
+});
